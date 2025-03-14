0b9dae95dccf5b503495494eb07389878cbe142b
Toan Nguyen Thanh <toannguyenthnh25@gmail.com>
2025-03-14 21:42:39 +0700

    fix comparions count

--- Sort.cpp
+++ Sort.cpp
@@ -4,14 +4,17 @@ using namespace std;
 
 void bubbleSort(int a[], int n, long long& comparisons) {
     for (int i = n - 1; i >= 0; i--) {
+        comparisons++;   
         bool swapped = false;
         for (int j = 0; j < i; j++) {
-            comparisons++;
+            comparisons++;   
+            comparisons++;   
             if (a[j] > a[j + 1]) {
                 swapped = true;
                 HoanVi(a[j], a[j + 1]);
             }
         }
+        comparisons++;   
         if (!swapped)
             return;
     }
@@ -22,27 +25,32 @@ void shakerSort(int a[], int n, long long& comparisons) {
     int right = n - 1;
     int k = 0;
     while (left <= right) {
+        comparisons++;  
         bool swapped = false;
         for (int j = left; j < right; j++) {
-            comparisons++;
+            comparisons++;   
+            comparisons++;   
             if (a[j] > a[j + 1]) {
                 swapped = true;
                 HoanVi(a[j], a[j + 1]);
                 k = j;
             }
         }
+        comparisons++;   
         if (!swapped)
             return;
         right = k;
         swapped = false;
         for (int j = right; j > left; j--) {
-            comparisons++;
+            comparisons++;   
+            comparisons++;   
             if (a[j] < a[j - 1]) {
                 swapped = true;
                 HoanVi(a[j], a[j - 1]);
                 k = j;
             }
         }
+        comparisons++;   
         if (!swapped)
             return;
         left = k;
@@ -51,9 +59,11 @@ void shakerSort(int a[], int n, long long& comparisons) {
 
 void selectionSort(int a[], int n, long long& comparisons) {
     for (int i = 0; i < n; i++) {
+        comparisons++;   
         int minIndex = i;
         for (int j = i + 1; j < n; j++) {
-            comparisons++;
+            comparisons++;   
+            comparisons++;   
             if (a[j] < a[minIndex]) {
                 minIndex = j;
             }
@@ -64,10 +74,12 @@ void selectionSort(int a[], int n, long long& comparisons) {
 
 void insertionSort(int a[], int n, long long& comparisons) {
     for (int i = 1; i < n; i++) {
+        comparisons++;   
         int key = a[i];
         int j = i - 1;
         while (j >= 0) {
-            comparisons++;
+            comparisons++;  
+            comparisons++;   
             if (a[j] > key) {
                 a[j + 1] = a[j];
                 j--;
@@ -84,16 +96,17 @@ int partition(int a[], int first, int last, long long& comparisons) {
     int i = first;
     int j = last;
     while (i <= j) {
+        comparisons++;  
         while (a[i] < pivot) {
-            comparisons++;
+            comparisons++;  
             i++;
         }
         while (a[j] > pivot) {
-            comparisons++;
+            comparisons++;  
             j--;
         }
+        comparisons++;   
         if (i <= j) {
-            comparisons++;
             HoanVi(a[i], a[j]);
             i++;
             j--;
@@ -104,21 +117,25 @@ int partition(int a[], int first, int last, long long& comparisons) {
 
 void quickSort(int a[], int first, int last, long long& comparisons) {
     int index = partition(a, first, last, comparisons);
+    comparisons++;   
     if (first < index - 1)
         quickSort(a, first, index - 1, comparisons);
+    comparisons++;   
     if (index < last)
         quickSort(a, index, last, comparisons);
 }
 
 void heapRebuild(int a[], int pos, int n, long long& comparisons) {
     while (2 * pos + 1 < n) {
+        comparisons++;  
         int j = 2 * pos + 1;
+        comparisons++;   
         if (j < n - 1) {
-            comparisons++;
+            comparisons++;   
             if (a[j] < a[j + 1])
                 j++;
         }
-        comparisons++;
+        comparisons++;   
         if (a[pos] >= a[j])
             return;
         HoanVi(a[pos], a[j]);
@@ -127,14 +144,17 @@ void heapRebuild(int a[], int pos, int n, long long& comparisons) {
 }
 
 void heapConstruct(int a[], int n, long long& comparisons) {
-    for (int i = (n - 1) / 2; i >= 0; i--)
+    for (int i = (n - 1) / 2; i >= 0; i--) {
+        comparisons++;   
         heapRebuild(a, i, n, comparisons);
+    }
 }
 
 void heapSort(int a[], int n, long long& comparisons) {
     heapConstruct(a, n, comparisons);
     int r = n - 1;
     while (r > 0) {
+        comparisons++;  
         HoanVi(a[0], a[r]);
         heapRebuild(a, 0, r, comparisons);
         r--;
@@ -146,29 +166,39 @@ void merge(int a[], int first, int mid, int last, long long& comparisons) {
     int n2 = last - mid;
     int *L = new int[n1];
     int *R = new int[n2];
-    for (int i = 0; i < n1; i++)
+    for (int i = 0; i < n1; i++) {
+        comparisons++;   
         L[i] = a[first + i];
-    for (int j = 0; j < n2; j++)
+    }
+    for (int j = 0; j < n2; j++) {
+        comparisons++;   
         R[j] = a[mid + j + 1];
+    }
     int i = 0;
     int j = 0;
     int k = first;
     while (i < n1 && j < n2) {
-        comparisons++;
+        comparisons += 2;  
+        comparisons++;   
         if (L[i] < R[j])
             a[k++] = L[i++];
         else
             a[k++] = R[j++];
     }
-    while (j < n2)
+    while (j < n2) {
+        comparisons++;  
         a[k++] = R[j++];
-    while (i < n1)
+    }
+    while (i < n1) {
+        comparisons++;  
         a[k++] = L[i++];
+    }
     delete[] L;
     delete[] R;
 }
 
 void mergeSort(int a[], int first, int last, long long& comparisons) {
+    comparisons++;   
     if (first < last) {
         int mid = first + (last - first) / 2;
         mergeSort(a, first, mid, comparisons);
@@ -179,11 +209,14 @@ void mergeSort(int a[], int first, int last, long long& comparisons) {
 
 void shellSort(int a[], int n, long long& comparisons) {
     for (int interval = n / 2; interval > 0; interval /= 2) {
+        comparisons++;   
         for (int i = interval; i < n; i++) {
+            comparisons++;   
             int temp = a[i];
             int j = i;
             while (j >= interval) {
-                comparisons++;
+                comparisons++;  
+                comparisons++;   
                 if (a[j - interval] > temp) {
                     a[j] = a[j - interval];
                     j -= interval;
@@ -198,15 +231,16 @@ void shellSort(int a[], int n, long long& comparisons) {
 
 int binarySearch(int a[], int first, int item, int last, long long& comparisons) {
     while (true) {
+        comparisons++;   
         if (first >= last) {
-            comparisons++;
+            comparisons++;   
             return (a[first] > item) ? first : first + 1;
         }
         int mid = (first + last) / 2;
-        comparisons++;
+        comparisons++;   
         if (a[mid] == item)
             return mid + 1;
-        comparisons++;
+        comparisons++;   
         if (a[mid] < item)
             first = mid + 1;
         else
@@ -217,11 +251,12 @@ int binarySearch(int a[], int first, int item, int last, long long& comparisons)
 void binaryInsertionSort(int a[], int n, long long& comparisons) {
     int i, loc, j, k, selected;
     for (i = 1; i < n; ++i) {
+        comparisons++;   
         j = i - 1;
         selected = a[i];
         loc = binarySearch(a, 0, selected, j, comparisons);
         while (j >= loc) {
-            comparisons++;
+            comparisons++;  
             a[j + 1] = a[j];
             j--;
         }
@@ -232,29 +267,39 @@ void binaryInsertionSort(int a[], int n, long long& comparisons) {
 void countingSort(int a[], int n, long long& comparisons) {
     int max = a[0];
     for (int i = 1; i < n; i++) {
-        comparisons++;
+        comparisons++;   
+        comparisons++;   
         if (a[i] > max)
             max = a[i];
     }
 
     int *count = new int[max + 1];
-    for (int i = 0; i <= max; i++)
+    for (int i = 0; i <= max; i++) {
+        comparisons++;   
         count[i] = 0;
+    }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
+        comparisons++;   
         count[a[i]]++;
+    }
 
-    for (int i = 1; i <= max; i++)
+    for (int i = 1; i <= max; i++) {
+        comparisons++;   
         count[i] += count[i - 1];
+    }
 
     int *temp = new int[n];
     for (int i = 0; i < n; i++) {
+        comparisons++;   
         temp[count[a[i]] - 1] = a[i];
         count[a[i]]--;
     }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
+        comparisons++;   
         a[i] = temp[i];
+    }
     delete[] count;
     delete[] temp;
 }
@@ -264,21 +309,31 @@ void radixSort(int a[], int n, long long& comparisons) {
     int m = a[0], exp = 1;
 
     for (int i = 0; i < n; i++) {
-        comparisons++;
+        comparisons++;   
+        comparisons++;   
         if (a[i] > m)
             m = a[i];
     }
 
     while (m / exp > 0) {
+        comparisons++;  
         int bucket[10] = { 0 };
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
+            comparisons++;   
             bucket[a[i] / exp % 10]++;
-        for (int i = 1; i < 10; i++)
+        }
+        for (int i = 1; i < 10; i++) {
+            comparisons++;   
             bucket[i] += bucket[i - 1];
-        for (int i = n - 1; i >= 0; i--)
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            comparisons++;   
             b[--bucket[a[i] / exp % 10]] = a[i];
-        for (int i = 0; i < n; i++)
+        }
+        for (int i = 0; i < n; i++) {
+            comparisons++;   
             a[i] = b[i];
+        }
         exp *= 10;
     }
     delete[] b;
@@ -289,25 +344,32 @@ void flashSort(int a[], int n, long long& comparisons) {
     int max = 0;
     int m = int(0.45 * n);
     int* l = new int[m];
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; i++) {
+        comparisons++;   
         l[i] = 0;
+    }
     for (int i = 1; i < n; i++) {
-        comparisons++;
+        comparisons++;   
+        comparisons++;   
         if (a[i] < minVal)
             minVal = a[i];
-        comparisons++;
+        comparisons++;   
         if (a[i] > a[max])
             max = i;
     }
+    comparisons++;   
     if (a[max] == minVal)
         return;
     double c1 = (double)(m - 1) / (a[max] - minVal);
     for (int i = 0; i < n; i++) {
+        comparisons++;   
         int k = int(c1 * (a[i] - minVal));
         ++l[k];
     }
-    for (int i = 1; i < m; i++)
+    for (int i = 1; i < m; i++) {
+        comparisons++;   
         l[i] += l[i - 1];
+    }
     HoanVi(a[max], a[0]);
     int nmove = 0;
     int j = 0;
@@ -315,13 +377,17 @@ void flashSort(int a[], int n, long long& comparisons) {
     int t = 0;
     int flash;
     while (nmove < n - 1) {
+        comparisons++;  
         while (j > l[k] - 1) {
+            comparisons++; 
             j++;
             k = int(c1*(a[j] - minVal));
         }
         flash = a[j];
+        comparisons++; 
         if (k < 0) break;
         while (j != l[k]) {
+            comparisons++; 
             k = int(c1*(flash - minVal));
             int hold = a[t = --l[k]];
             a[t] = flash;
